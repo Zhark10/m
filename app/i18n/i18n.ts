d0dbf1ffa782aa4fa34938b49b04c9e65afe2189
Zhark10 <verygoodman18@yandex.ru>
2020-12-27 03:33:19 +0300

    intl changed

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -2,9 +2,10 @@ import * as Localization from "expo-localization"
 import i18n from "i18n-js"
 
 const en = require("./en")
-const ja = require("./ja")
+const ru = require("./ru")
 
 i18n.fallbacks = true
-i18n.translations = { en, ja }
+i18n.translations = { en, ru }
 
-i18n.locale = Localization.locale || "en"
+// i18n.locale = Localization.locale || "ru"
+i18n.locale = "ru"
