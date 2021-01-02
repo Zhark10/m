6f54089ed02c4c4b8ede0f51ff5afb1429401fc7
Zhark10 <verygoodman18@yandex.ru>
2021-01-02 19:02:26 +0300

    added 3d cube to play

---
+++ app/screens/map/MapScreen-Animations/Cube/index.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import { color } from "../../../../theme/color"
+import { screenWidth } from "../../../../utils/screen"
+
+import ZBox from "./ZBox"
+import ZSvg from "./ZSvg"
+
+const cubeSize = screenWidth / 3
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    height: cubeSize,
+    justifyContent: "center",
+    width: cubeSize,
+  },
+})
+
+const canvas = {
+  x: cubeSize,
+  y: cubeSize,
+  z: cubeSize,
+}
+
+export const Cube = () => {
+  return (
+    <View style={styles.container}>
+      <ZSvg canvas={canvas}>
+        <ZBox
+          width={0.5}
+          height={0.5}
+          depth={0.5}
+          front={color.palette.gold}
+          back={color.palette.gold}
+          left={color.palette.gold}
+          right={color.palette.gold}
+          top={color.palette.gold}
+          bottom={color.palette.gold}
+        />
+      </ZSvg>
+    </View>
+  )
+}
