80a1e52e56f7dfcf018773c7d67a0e59855deba5
Zhark10 <verygoodman18@yandex.ru>
2021-01-08 02:04:59 +0300

    place selection box upd

--- app/screens/map/MapScreen-Components/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection-Styles.ts
+++ app/screens/map/MapScreen-Components/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection-Styles.ts
@@ -1,5 +1,31 @@
+/* eslint-disable react-native/sort-styles */
+/* eslint-disable react-native/no-color-literals */
 import { StyleSheet } from "react-native"
+import { color, spacing, typography } from "../../../../../../theme"
+import { screenHeight } from "../../../../../../utils/screen"
+
+export const COONTAINER_HEIGHT = screenHeight / 3 - 128
 
 export const Step2Styles = StyleSheet.create({
+  BOX: {
+    borderRadius: 8,
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+  CONTAINER: {
+    alignItems: 'center',
+    flex: 1,
+    height: COONTAINER_HEIGHT,
+    justifyContent: 'center',
+    marginVertical: spacing[3],
+  },
+  PLACE_TITLE: {
+    color: color.palette.white,
+    fontFamily: typography.primary.book,
+    textAlign: 'center',
+    fontSize: 20,
+    lineHeight: 32,
+    padding: spacing[2],
+  },
 
 })
--- app/screens/map/MapScreen-Components/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection.tsx
+++ app/screens/map/MapScreen-Components/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection.tsx
@@ -1,9 +1,40 @@
-import React, { FC } from "react"
+import React, { FC, useRef } from "react"
+import { View, Text } from "react-native"
 import { StepCard } from "../StepCard/StepCard"
+import { COONTAINER_HEIGHT, Step2Styles } from "./Step2_PlaceSelection-Styles"
+import Carousel from 'react-native-snap-carousel'
+import { screenWidth } from "../../../../../../utils/screen"
+import { useStores } from "../../../../../../models"
 
 export const Step2: FC = () => {
+  const renderItem = ({ item, index }) => (
+    <View
+      key={index}
+      style={Step2Styles.BOX}
+    >
+      <Text style={Step2Styles.PLACE_TITLE}>{item.organizationName}</Text>
+    </View>
+  )
+
+  const carouselRef = useRef(null)
+
+  const { city: { availablePlaces } } = useStores()
+
   return (
     <StepCard title="mapScreen.game_steps.step_2" theme="light" size="small">
+      <View
+        style={Step2Styles.CONTAINER}
+      >
+        <Carousel
+          ref={carouselRef}
+          data={availablePlaces}
+          renderItem={renderItem}
+          sliderWidth={screenWidth / 2}
+          layout="tinder"
+          layoutCardOffset={COONTAINER_HEIGHT}
+          itemWidth={screenWidth / 2 - 42}
+        />
+      </View>
     </StepCard>
   )
 }
