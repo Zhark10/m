601bc948a32062dddcdd2d91e40c19e1765dfdf5
Zhark10 <verygoodman18@yandex.ru>
2021-01-11 00:03:34 +0300

    root store minor changes

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -1,7 +1,7 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
 import { Auth } from "../entities/Auth/Models"
 import { City } from "../entities/City/Store"
-import { Game } from "../entities/Game/Models"
+import { Game } from "../entities/Game/Store"
 import { Profile } from "../entities/Profile/Models"
 
 const NestedStores = {
@@ -10,12 +10,24 @@ const NestedStores = {
     currentPlace: null,
     availablePlaces: City.fake.availablePlaces
   }),
+  game: types.optional(Game.Store, {
+    gameProgress: {
+      step1_DiceResult: {
+        first: null,
+        second: null
+      },
+      step2_SelectedPlaceToBuild: null,
+      step3_IsBuildStarted: null,
+      step4_IsBuildFinished: null,
+    },
+    currentActivePlayer: null,
+    isLoaded: true,
+  })
 }
 
 const SimpleModels = {
   auth: types.maybeNull(Auth.Model),
   profile: types.maybeNull(Profile.Model),
-  game: types.maybeNull(Game.Model),
 }
 
 export const RootStoreModel = types.model("RootStore", {
