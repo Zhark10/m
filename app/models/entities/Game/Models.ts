c5d8458e3d72b99b7e6d41bad0cf8cbe76a3ab16
Zhark10 <verygoodman18@yandex.ru>
2021-01-11 00:02:37 +0300

    changed game entity

--- app/models/entities/Game/Models.ts
+++ app/models/entities/Game/Models.ts
@@ -1,28 +1,13 @@
 import { types } from "mobx-state-tree"
 
-const DiceResultModel = types.union(
-  types.literal(1),
-  types.literal(2),
-  types.literal(3),
-  types.literal(4),
-  types.literal(5),
-  types.literal(6)
-)
+const Cube = types.model({
+  first: types.maybeNull(types.number),
+  second: types.maybeNull(types.number),
+})
 
-const GameProgressModel = types.model({
-  step1_DiceResult: types.maybeNull(DiceResultModel),
-  step2_SelectedPlaceToBuild: types.maybeNull(types.identifier),
+export const GameProgressModel = types.model("GameProgress", {
+  step1_DiceResult: types.maybeNull(Cube),
+  step2_SelectedPlaceToBuild: types.maybeNull(types.string),
   step3_IsBuildStarted: types.maybeNull(types.boolean),
   step4_IsBuildFinished: types.maybeNull(types.boolean),
 })
-
-const Model = types.model({
-  id: types.identifier,
-  currentActivePlayer: types.string,
-  gameProgress: GameProgressModel,
-  isLoaded: types.boolean,
-})
-
-export const Game = {
-  Model,
-}
--- app/models/entities/Game/Types.ts
+++ app/models/entities/Game/Types.ts
@@ -1,5 +1,5 @@
 import { Instance } from "mobx-state-tree"
-import { Game } from "./Models"
+import { Game } from "./Store"
 
-type TGameModel = typeof Game.Model
+type TGameModel = typeof Game.Store
 export interface TGame extends Instance<TGameModel> { }
