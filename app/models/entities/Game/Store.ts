01e5d811787612393c51a0fbcfce800071d94e42
Zhark10 <verygoodman18@yandex.ru>
2021-01-13 18:59:33 +0300

    added getters

--- app/models/entities/Game/Store.ts
+++ app/models/entities/Game/Store.ts
@@ -1,11 +1,28 @@
 import { types } from "mobx-state-tree"
 import { GameProgressModel } from "./Models"
 
-export const Store = types.model({
-  currentActivePlayer: types.maybeNull(types.string),
-  gameProgress: GameProgressModel,
-  isLoaded: types.boolean,
-})
+export const Store = types
+  .model({
+    currentActivePlayer: types.maybeNull(types.string),
+    gameProgress: GameProgressModel,
+    isLoaded: types.boolean,
+  })
+  .views(self => ({
+    // FOR DYNAMIC MESSAGE SHOW
+    get canBeCompletedStep1() {
+      const { first, second } = self.gameProgress.step1_DiceResult
+      return Boolean(first && second)
+    },
+    get canBeCompletedStep2() {
+      return Boolean(self.gameProgress.step2_SelectedPlaceToBuild.placeId)
+    },
+    get canBeCompletedStep3() {
+      return Boolean(self.gameProgress.step3_IsBuildStarted.isPlaceBuildStart)
+    },
+    get canBeCompletedStep4() {
+      return Boolean(self.gameProgress.step4_IsBuildFinished.IsBuildFinished)
+    },
+  }))
   .actions(self => ({
     saveDiceResult(cubeNumber: "first" | "second", value: number) {
       self.gameProgress.step1_DiceResult[cubeNumber] = value
@@ -13,7 +30,8 @@ export const Store = types.model({
     resetDiceResult() {
       self.gameProgress.step1_DiceResult = {
         first: 0,
-        second: 0
+        second: 0,
+        isCompleted: false
       }
     }
   }))
