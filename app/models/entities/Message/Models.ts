d3e831b7ef0617282088c53643bc76a552ae7b21
Zhark10 <verygoodman18@yandex.ru>
2021-01-14 00:02:00 +0300

    message entity created

---
+++ app/models/entities/Message/Models.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { types } from "mobx-state-tree"
+
+const Model = types
+  .model({
+    title: types.maybeNull(types.string),
+    description: types.maybeNull(types.string),
+    buttonText: types.maybeNull(types.string)
+  })
+  .actions(self => ({
+    showMessage(message) {
+      self.title = message.title
+      self.description = message.description
+      self.buttonText = message.buttonText
+    },
+    hideMessage() {
+      self.title = null
+      self.description = null
+      self.buttonText = null
+    }
+  }))
+  .views(self => ({
+    get isShow() {
+      return Boolean(self.title)
+    }
+  }))
+
+export const Message = {
+  Model,
+}
---
+++ app/models/entities/Message/Types.ts
@@ -0,0 +1,5 @@
+import { Instance } from "mobx-state-tree"
+import { Message } from "./Models"
+
+type TMessageModel = typeof Message.Model
+export interface TMessage extends Instance<TMessageModel> { }
