2f8032f5885d2ec731c13b7c47e35af3be8765e0
Zhark10 <verygoodman18@yandex.ru>
2021-01-14 00:04:10 +0300

    animated message styles added

--- app/screens/map/MapScreen-Components/AnimatedMessage/AnimatedMessage-Styles.ts
+++ app/screens/map/MapScreen-Components/AnimatedMessage/AnimatedMessage-Styles.ts
@@ -1,18 +1,30 @@
 /* eslint-disable react-native/no-color-literals */
 import { StyleSheet } from "react-native"
+import { color, spacing, typography } from "../../../../theme"
 import { screenHeight, screenWidth } from "../../../../utils/screen"
 
-const CONTAINER_HEIGHT = 2 / 3 * screenHeight
+const CONTAINER_HEIGHT = 2 / 3 * screenHeight - 72
 
 export const AnimatedMessageStyles = StyleSheet.create({
   BLUR_VIEW: {
     alignItems: 'center',
-    flexDirection: 'row',
     height: CONTAINER_HEIGHT,
-    justifyContent: 'space-evenly',
+    justifyContent: 'flex-end',
     position: 'absolute',
     top: 0,
     width: screenWidth,
+  },
+  CARD_BUTTON: {
+    alignSelf: 'center',
+    backgroundColor: color.palette.gold,
+    height: 46,
+    margin: spacing[5],
+    width: '90%',
+  },
+  CARD_BUTTON_TEXT: {
+    color: color.palette.black,
+    fontFamily: typography.primary.book,
+    fontSize: 24,
   },
   CONTAINER: {
     elevation: 5,
@@ -25,8 +37,24 @@ export const AnimatedMessageStyles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    top: 0,
     width: screenWidth,
     zIndex: 9999,
+  },
+  ICON: {
+    color: color.palette.gold,
+    fontSize: 72,
+    padding: spacing[4],
+  },
+  MESSAGE_DESCRIPTION: {
+    color: color.palette.black,
+    fontFamily: typography.primary.book,
+    fontSize: 32,
+    padding: spacing[4],
+  },
+  MESSAGE_TITLE: {
+    color: color.palette.black,
+    fontFamily: typography.primary.bold,
+    fontSize: 42,
+    padding: spacing[3],
   },
 })
