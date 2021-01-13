09a3bbe93cdc793bab1bb2111106cdf7c1e6c092
Zhark10 <verygoodman18@yandex.ru>
2021-01-14 00:04:39 +0300

    fixed custom options states

--- app/screens/map/MapScreen-Components/AnimatedMessage/AnimatedMessage.tsx
+++ app/screens/map/MapScreen-Components/AnimatedMessage/AnimatedMessage.tsx
@@ -4,6 +4,8 @@ import { BlurView } from "@react-native-community/blur"
 import { AnimatedMessageStyles } from "./AnimatedMessage-Styles"
 import { useAnimatedMessage } from "./AnimatedMessage-VM"
 import { observer } from "mobx-react-lite"
+import { Button, Text } from "../../../../components"
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export type TAnimatedMessageProps = {
 
@@ -11,17 +13,30 @@ export type TAnimatedMessageProps = {
 
 export const AnimatedMessage: FC<TAnimatedMessageProps> = observer(() => {
   const vm = useAnimatedMessage()
-  const { data: { animationStyles }, methods: { hideMessage } } = vm
+  const { data: { animationStyles, message }, methods: { hideMessage } } = vm
 
   return (
-    <Animated.View style={AnimatedMessageStyles.CONTAINER}>
+    <Animated.View style={[AnimatedMessageStyles.CONTAINER, animationStyles.styleMessageShow]}>
       <BlurView
         style={AnimatedMessageStyles.BLUR_VIEW}
-        blurType="dark"
-        blurAmount={10}
+        blurType="light"
+        blurAmount={20}
         reducedTransparencyFallbackColor="white"
       >
-        
+        <MaterialCommunityIcons
+          name="check-decagram"
+          style={AnimatedMessageStyles.ICON}
+        />
+        <Text style={AnimatedMessageStyles.MESSAGE_TITLE}>{message?.title}</Text>
+        <Text style={AnimatedMessageStyles.MESSAGE_DESCRIPTION}>{message?.description}</Text>
+        <Button
+          style={AnimatedMessageStyles.CARD_BUTTON}
+          onPress={hideMessage}
+        >
+          <Text style={AnimatedMessageStyles.CARD_BUTTON_TEXT} >
+            {message?.buttonText}
+          </Text>
+        </Button>
       </BlurView>
     </Animated.View>
   )
--- app/screens/map/MapScreen-Components/CustomOptions/CustomOptions-VM.ts
+++ app/screens/map/MapScreen-Components/CustomOptions/CustomOptions-VM.ts
@@ -1,20 +1,20 @@
 import { useStores } from "../../../../models"
 
 export const useCustomOptions = () => {
-  const { game } = useStores()
+  const { game: { gameProgress } } = useStores()
 
   const OPTIONS = [
     {
       tx: "mapScreen.options.step_1_completed_text",
-      complete: game.canBeCompletedStep1
+      complete: gameProgress.step1_DiceResult.isCompleted
     },
     {
       tx: "mapScreen.options.step_2_completed_text",
-      complete: game.canBeCompletedStep2
+      complete: gameProgress.step2_SelectedPlaceToBuild.isCompleted
     },
     {
       tx: "mapScreen.options.step_3_completed_text",
-      complete: game.canBeCompletedStep3
+      complete: gameProgress.step3_IsBuildStarted.isCompleted
     },
   ]
 
