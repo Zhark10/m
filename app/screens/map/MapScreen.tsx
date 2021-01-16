1b3d2dabac1cdd30126ed099fd6cd64c1357bbb2
Zhark10 <verygoodman18@yandex.ru>
2021-01-16 03:08:50 +0300

    map view updated

--- app/screens/map/MapScreen.tsx
+++ app/screens/map/MapScreen.tsx
@@ -1,9 +1,10 @@
-import React from "react"
+/* eslint-disable react-native/no-inline-styles */
+import React, { Fragment } from "react"
 import { View } from "react-native"
 import { observer } from "mobx-react-lite"
 import { Screen } from "../../components"
 import { color } from "../../theme"
-import MapView, { Marker, Polygon, Polyline } from "react-native-maps"
+import MapView, { Circle, Marker, Polygon, Polyline } from "react-native-maps"
 import { MapScreenStyles } from "./MapScreen-Styles"
 import { customMapStyles } from "../welcome/WelcomeScreen-CustomMapStyles"
 import { useMap } from "./MapScreen-VM"
@@ -14,20 +15,21 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import { screenHeight, screenWidth } from "../../utils/screen"
 import { CustomOptions } from "./MapScreen-Components/CustomOptions/CustomOptions"
 import { AnimatedMessage } from "./MapScreen-Components/AnimatedMessage/AnimatedMessage"
+import getDistance from 'geolib/es/getDistance'
 
 const ASPECT_RATIO = screenWidth / screenHeight
-const LATITUDE_DELTA = 0.075
-
-const defaultCoordinates = {
-  latitude: 56.62830507073426,
-  longitude: 47.895421717849814,
-  latitudeDelta: LATITUDE_DELTA,
-  longitudeDelta: LATITUDE_DELTA * ASPECT_RATIO,
-}
+const LATITUDE_DELTA = 0.12
 
 export const MapScreen = observer(function MapScreen() {
   const vm = useMap()
-  const { data: { animationStyles, availablePlaces, isMapTouched, mapViewRef }, methods } = vm
+  const { data: { animationStyles, places, isMapTouched, mapViewRef, radiusInMeters }, methods } = vm
+
+  const defaultCoordinates = {
+    latitude: 56.62830507073426,
+    longitude: 47.895421717849814,
+    latitudeDelta: radiusInMeters ? LATITUDE_DELTA * radiusInMeters : LATITUDE_DELTA,
+    longitudeDelta: radiusInMeters ? LATITUDE_DELTA * ASPECT_RATIO * radiusInMeters : LATITUDE_DELTA * ASPECT_RATIO,
+  }
 
   return (
     <View testID="MapScreen" style={MapScreenStyles.FULL}>
@@ -49,31 +51,51 @@ export const MapScreen = observer(function MapScreen() {
               <Fontisto name="person" style={MapScreenStyles.ME_ICON} />
             </Marker>
             {
-              availablePlaces.map((place) => (
-                <Marker key={place.organizationName} title={place.organizationName} description={place.organizationOwner} coordinate={place.coordinates} style={MapScreenStyles.BUILDING}>
-                  <FontAwesome5
-                    name="building"
-                    style={MapScreenStyles.BUILDING_ICON}
-                  />
-                </Marker>
-              ))
+              places.map((place) => {
+                const distance = getDistance(place.coordinates, defaultCoordinates)
+                const isEntersTheArea = distance <= radiusInMeters
+                return (
+                  <Fragment key={place.organizationName}>
+                    <Marker
+                      title={place.organizationName}
+                      description={place.organizationOwner}
+                      coordinate={place.coordinates}
+                      style={[
+                        MapScreenStyles.BUILDING,
+                        {
+                          backgroundColor: isEntersTheArea ? color.palette.gold : color.palette.black,
+                          borderWidth: isEntersTheArea ? 1 : 0
+                        },
+                      ]}>
+                      <FontAwesome5
+                        name="building"
+                        style={[
+                          MapScreenStyles.BUILDING_ICON,
+                          { color: isEntersTheArea ? color.palette.black : color.palette.white }
+                        ]}
+                      />
+                    </Marker>
+                    {
+                      isEntersTheArea &&
+                      <Polyline
+                        key={place.id}
+                        coordinates={[defaultCoordinates, place.coordinates]}
+                        strokeColor={color.palette.black}
+                        strokeWidth={1}
+                      />
+                    }
+                  </Fragment>
+                )
+              })
             }
-            <Polygon
-              coordinates={[...availablePlaces.map(place => place.coordinates), availablePlaces[0].coordinates]}
-              strokeColor={color.palette.opacity.gold32}
-              fillColor={color.palette.opacity.gold32}
-              strokeWidth={2}
-            />
             {
-              availablePlaces.map((place) => (
-                <Polyline
-                  key={place.id}
-                  coordinates={[defaultCoordinates, place.coordinates]}
-                  strokeColor={color.palette.gold}
-                  fillColor={color.palette.gold}
-                  strokeWidth={2}
-                />
-              ))
+              <Circle
+                center={defaultCoordinates}
+                radius={radiusInMeters}
+                strokeWidth={2}
+                strokeColor={color.palette.gold}
+                fillColor={color.palette.opacity.gold32}
+              />
             }
           </MapView>
         </Animated.View>
