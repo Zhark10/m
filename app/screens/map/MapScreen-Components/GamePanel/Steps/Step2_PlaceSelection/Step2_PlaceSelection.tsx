59c9d8dd8497e7ab70c08ce29e93210483b3b634
Zhark10 <verygoodman18@yandex.ru>
2021-01-17 02:42:59 +0300

    Place selection component changed

--- app/screens/map/MapScreen-Components/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection.tsx
+++ app/screens/map/MapScreen-Components/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { FC, useCallback, useRef } from "react"
-import { View } from "react-native"
+import { TouchableOpacity, View } from "react-native"
 import { StepCard } from "../StepCard/StepCard"
-import { COONTAINER_HEIGHT, Step2Styles } from "./Step2_PlaceSelection-Styles"
+import { COONTAINER_HEIGHT, SLIDER_WIDTH, Step2Styles } from "./Step2_PlaceSelection-Styles"
 import Carousel from 'react-native-snap-carousel'
-import { screenWidth } from "../../../../../../utils/screen"
 import { useStores } from "../../../../../../models"
-import { Button, Text } from "../../../../../../components"
+import { Text } from "../../../../../../components"
 import { getCardColorByCost } from "../../../../../../utils/helpers/get-color"
+import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5"
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome"
+import { color } from "../../../../../../theme"
 
 export const Step2: FC = () => {
   const renderItem = ({ item, index }) => (
@@ -16,23 +19,28 @@ export const Step2: FC = () => {
     >
       <Text style={[Step2Styles.PLACE_TITLE, { backgroundColor: getCardColorByCost(item.cost) }]}>{item.organizationName}</Text>
       <View style={Step2Styles.PLACE_CONTAINER}>
-        <View style={Step2Styles.PLACE_ITEM}>
-          <Text style={Step2Styles.PLACE_ITEM_TITLE} tx={"mapScreen.game_steps.step_2.current_owner"} />
-          <Text style={Step2Styles.PLACE_ITEM_INFO}>{item.organizationOwner}</Text>
+        <View style={Step2Styles.PLACE_BOX}>
+          <View style={Step2Styles.PLACE_ITEM}>
+            <Text style={Step2Styles.PLACE_ITEM_TITLE} tx={"mapScreen.game_steps.step_2.current_owner"} />
+            <Text style={Step2Styles.PLACE_ITEM_INFO}>{item.organizationOwner}</Text>
+          </View>
+          <View style={Step2Styles.PLACE_ITEM}>
+            <Text style={Step2Styles.PLACE_ITEM_TITLE} tx={"mapScreen.game_steps.step_2.current_owner"} />
+            <Text style={Step2Styles.PLACE_ITEM_INFO}>{item.organizationOwner}</Text>
+          </View>
+          <View style={Step2Styles.PLACE_ITEM}>
+            <Text style={Step2Styles.PLACE_ITEM_TITLE} tx={"mapScreen.game_steps.step_2.current_owner"} />
+            <Text style={Step2Styles.PLACE_ITEM_INFO}>{item.organizationOwner}</Text>
+          </View>
         </View>
-        <View style={Step2Styles.PLACE_ITEM}>
-          <Text style={Step2Styles.PLACE_COST}>{item.cost} $</Text>
+        <View style={Step2Styles.PLACE_BOX}>
+          <View style={Step2Styles.PLACE_ITEM}>
+            <Text style={Step2Styles.PLACE_COST}>{item.cost} $</Text>
+          </View>
         </View>
       </View>
-      <Button
-        style={Step2Styles.CARD_BUTTON}
-        onPress={() => ({})}
-      >
-        <Text>
-          <Text style={Step2Styles.CARD_BUTTON_TEXT} tx="mapScreen.game_steps.step_2.go_button" />
-          {/* <Text style={Step2Styles.CARD_BUTTON_PLACE_TEXT}> {item.organizationName}</Text> */}
-        </Text>
-      </Button>
+      {/* <View style={{width: '100%', height: 1, marginBottom: 4, backgroundColor: color.palette.opacity.black40}}/> */}
+      <Text style={Step2Styles.COPYRIGHT}>© Somecity, 2020</Text>
     </View>
   )
 
@@ -58,18 +66,32 @@ export const Step2: FC = () => {
         <View
           style={Step2Styles.CONTAINER}
         >
-          <Carousel
-            ref={carouselRef}
-            data={availablePlaces}
-            renderItem={renderItem}
-            sliderWidth={screenWidth}
-            onSnapToItem={onSnapToItem}
-            layout="stack"
-            layoutCardOffset={COONTAINER_HEIGHT}
-            sliderHeight={COONTAINER_HEIGHT + 24}
-            itemHeight={COONTAINER_HEIGHT - 24}
-            itemWidth={screenWidth - 42}
-          />
+          <View style={Step2Styles.CARD_CONTENT}>
+            <View style={Step2Styles.CARD_CONTENT_LEFT_BOX}>
+              <View style={Step2Styles.INVISIBLE_BOX}/>
+              <TouchableOpacity style={Step2Styles.CARD_CONTENT_LEFT_BOX_LOCATION_ICON_BUTTON} onPress={() => null}>
+                <FontAwesome5Icon name='location-arrow' style={Step2Styles.CARD_CONTENT_LEFT_BOX_ICON} />
+                <FontAwesome5Icon name='angle-double-right' style={[Step2Styles.CARD_CONTENT_LEFT_BOX_ICON, { color: color.palette.opacity.black72, paddingLeft: 12, }]} />
+              </TouchableOpacity>
+              <TouchableOpacity style={Step2Styles.CARD_CONTENT_LEFT_BOX_ICON_BUTTON} onPress={() => null}>
+                <FontAwesomeIcon name='repeat' style={[Step2Styles.CARD_CONTENT_LEFT_BOX_ICON, { color: color.palette.opacity.black72 }]} />
+                <FontAwesome5Icon name='dice-d6' style={Step2Styles.CARD_CONTENT_LEFT_BOX_ICON} />
+              </TouchableOpacity>
+              <Text style={Step2Styles.ROLL_DICE_REPEAT_TEXT}>-150$</Text>
+            </View>
+            <Carousel
+              ref={carouselRef}
+              data={availablePlaces}
+              renderItem={renderItem}
+              sliderWidth={SLIDER_WIDTH}
+              onSnapToItem={onSnapToItem}
+              layout="stack"
+              sliderHeight={COONTAINER_HEIGHT + 24}
+              itemHeight={COONTAINER_HEIGHT - 24}
+              itemWidth={SLIDER_WIDTH - 22}
+            />
+          </View>
+
         </View>
       }
     />
