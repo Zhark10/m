55fb3198d16feb22b300594659c49c4d479fb6a5
Zhark10 <verygoodman18@yandex.ru>
2021-01-27 00:29:31 +0300

    minor styles changes

--- app/screens/map/MapScreen-Elements/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection-Styles.ts
+++ app/screens/map/MapScreen-Elements/GamePanel/Steps/Step2_PlaceSelection/Step2_PlaceSelection-Styles.ts
@@ -145,13 +145,13 @@ export const Step2Styles = StyleSheet.create({
     marginVertical: spacing[3],
   },
   INFO_DESCRIPTION: {
-    color: color.palette.white,
+    color: color.palette.black,
     fontFamily: typography.primary.book,
     fontSize: 16,
     textAlign: "center",
   },
   INFO_TITLE: {
-    color: color.palette.white,
+    color: color.palette.black,
     fontFamily: typography.primary.bold,
     fontSize: 20,
     lineHeight: 32,
--- app/screens/map/MapScreen-Elements/GamePanel/Steps/StepCard/StepCard-Styles.ts
+++ app/screens/map/MapScreen-Elements/GamePanel/Steps/StepCard/StepCard-Styles.ts
@@ -35,7 +35,7 @@ export const StepCardStyles = StyleSheet.create({
     width: screenWidth / 2,
   },
   TITLE: {
-    color: color.palette.white,
+    color: color.palette.black,
     fontFamily: typography.primary.bold,
     fontSize: 24,
     lineHeight: 32,
