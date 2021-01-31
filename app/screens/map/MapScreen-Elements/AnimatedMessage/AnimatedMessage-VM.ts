cbdaf62d57051e5ea5957219dd351488001885d9
Zhark10 <verygoodman18@yandex.ru>
2021-02-01 00:20:29 +0300

    add title text separator for custom styles part of text in animated message

--- app/screens/map/MapScreen-Elements/AnimatedMessage/AnimatedMessage-VM.ts
+++ app/screens/map/MapScreen-Elements/AnimatedMessage/AnimatedMessage-VM.ts
@@ -1,6 +1,17 @@
 import { useStores } from "../../../../models"
 import { AnimatedMessageAnimations } from "./AnimatedMessage-Animations"
 
+export const TEXT_SEPARATOR = {
+  TITLE: {
+    TO_STYLED_TEXT: 'TITLE_TO_STYLED_TEXT',
+    TO_ICON: 'TITLE_TO_ICON',
+  },
+  DESCRIPTION: {
+    TO_STYLED_TEXT: 'DESCRIPTION_TO_STYLED_TEXT',
+    TO_ICON: 'DESCRIPTION_TO_ICON',
+  },
+}
+
 export const useAnimatedMessage = () => {
   const { styleMessageShow } = AnimatedMessageAnimations.useMessages()
   const { message, game } = useStores()
--- app/screens/map/MapScreen-Elements/GamePanel/Steps/Step1_Dice/Step1_Dice-VM.ts
+++ app/screens/map/MapScreen-Elements/GamePanel/Steps/Step1_Dice/Step1_Dice-VM.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { useStores } from "../../../../../../models"
+import { TEXT_SEPARATOR } from "../../../AnimatedMessage/AnimatedMessage-VM"
 
 export const useStep1 = () => {
   const { game } = useStores()
@@ -15,8 +16,8 @@ export const useStep1 = () => {
     function pushMessage() {
       if (!game.canBeCompletedStep1) return
       const message = {
-        title: `И у тебя выпадает ${game.rollTheDiceResult} очков`,
-        description: `А это означает, что в радиусе ${game.calculatedRadius} км. ты можешь выбрать следующее место для постройки!`,
+        title: `И у тебя выпадает ${TEXT_SEPARATOR.TITLE.TO_STYLED_TEXT}${game.rollTheDiceResult}${TEXT_SEPARATOR.TITLE.TO_STYLED_TEXT} очков`,
+        description: `А это означает, что в радиусе ${TEXT_SEPARATOR.DESCRIPTION.TO_STYLED_TEXT}${game.calculatedRadius}${TEXT_SEPARATOR.DESCRIPTION.TO_STYLED_TEXT} км. ты можешь выбрать следующее место для постройки!`,
         buttonText: "Хорошо",
       }
       showMessage(message)
