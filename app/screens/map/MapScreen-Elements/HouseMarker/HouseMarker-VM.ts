a766d5f9433c9820705d7b1aa3e2da84676b0871
Zhark10 <verygoodman18@yandex.ru>
2021-02-18 18:52:34 +0300

    house marker vm

---
+++ app/screens/map/MapScreen-Elements/HouseMarker/HouseMarker-VM.ts
@@ -0,0 +1,14 @@
+import { useNavigation } from "@react-navigation/native"
+import { useStores } from "../../../../models"
+
+export const useHouseMarker = () => {
+  const navigation = useNavigation()
+  const { profile } = useStores()
+
+  return {
+    data: {
+      profile,
+    },
+    methods: { },
+  }
+}
--- app/screens/map/MapScreen-Elements/HouseMarker/HouseMarker.tsx
+++ app/screens/map/MapScreen-Elements/HouseMarker/HouseMarker.tsx
@@ -2,8 +2,12 @@ import React, { FC } from "react"
 import { View } from "react-native"
 import { observer } from "mobx-react-lite"
 import { HouseMarkerStyles } from "./HouseMarker-Styles"
+import { useHouseMarker } from "./HouseMarker-VM"
 
 export const HouseMarker: FC = observer(() => {
+  const vm = useHouseMarker()
+  const { data: { profile } } = vm
+
   return (
     <View style={HouseMarkerStyles.MARKER_CONTAINER}>
       <View style={HouseMarkerStyles.CIRCLE} />
