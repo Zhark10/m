a81204451bef0c491cbad704df5118a8ddf223fe
Zhark10 <verygoodman18@yandex.ru>
2021-02-20 00:00:47 +0300

    go to coordinate and come back animation hook

--- app/models/entities/AnimationEvent/AnimationHooks.ts
+++ app/models/entities/AnimationEvent/AnimationHooks.ts
@@ -1,7 +1,36 @@
+import { useCallback, useEffect } from "react"
+import { TCoordinates } from "../../../utils/types/coordinates"
 import { useStores } from "../../root-store/root-store-context"
 
-export const goToCoordinateAndBack = () => {
-  const {  } = useStores()
+type TCoordinatesToGoAndComeBack = {
+  startPosition: TCoordinates,
+  finishPosition: TCoordinates,
+}
 
-  
+const TIME_TO_FINISH_ANIMATION = 5000
+
+const useGoToCoordinateAndComeBack = () => {
+  const { animationEvent: { goAndComeBack, goAndComeBackActive, goAndComeBackAnimationFinish } } = useStores()
+
+  const goAndComeBackCb = useCallback(({ ...coordinates }: TCoordinatesToGoAndComeBack) => {
+    goAndComeBack(coordinates.startPosition, coordinates.finishPosition)
+  }, [])
+
+  useEffect(function animationFinish() {
+    if (goAndComeBackActive) {
+      const goAndComeBackAnimationFinishByTimer = setTimeout(() => {
+        goAndComeBackAnimationFinish()
+      }, TIME_TO_FINISH_ANIMATION)
+
+      return () => clearTimeout(goAndComeBackAnimationFinishByTimer)
+    }
+
+    return () => null
+  }, [goAndComeBackActive])
+
+  return { goAndComeBackCb }
+}
+
+export const useAnimationHooks = {
+  useGoToCoordinateAndComeBack
 }
