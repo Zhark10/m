d617e07e12ba68a99dd0863c5baa57fa8851e7dc
Zhark10 <verygoodman18@yandex.ru>
2021-02-21 23:49:24 +0300

    min changes

--- app/screens/map/MapScreen-Elements/GamePanel/GamePanel.tsx
+++ app/screens/map/MapScreen-Elements/GamePanel/GamePanel.tsx
@@ -30,7 +30,7 @@ export const GamePanel: FC<TGamePanelProps> = observer((props) => {
         reducedTransparencyFallbackColor="white"
       />
       <Animated.View style={[GamePanelStyles.GAME_SPACE, animationStyles.stepsStyle]}>
-        {gameSteps.slice().map((DynamicStepComponent, key) => (
+        {gameSteps.map((DynamicStepComponent, key) => (
           <Fragment key={key}>
             <DynamicStepComponent />
             <View style={GamePanelStyles.CARD_SEPARATOR} />
--- app/screens/map/MapScreen-Elements/GamePanel/Steps/Step1_Dice/Step1_Dice-VM.ts
+++ app/screens/map/MapScreen-Elements/GamePanel/Steps/Step1_Dice/Step1_Dice-VM.ts
@@ -19,8 +19,8 @@ export const useStep1 = () => {
         game.rollTheDiceResult > 10
           ? "Отлично!"
           : game.rollTheDiceResult > 8
-          ? "Неплохо!"
-          : "Упс... всего"
+            ? "Неплохо!"
+            : "Упс... всего"
       const message = {
         title: `${defaultTitlePart} ${TEXT_SEPARATOR.TITLE.TO_STYLED_TEXT}${game.rollTheDiceResult} очков${TEXT_SEPARATOR.TITLE.TO_STYLED_TEXT}`,
         description: `А это означает, что в радиусе ${TEXT_SEPARATOR.DESCRIPTION.TO_STYLED_TEXT}${game.calculatedRadius} км.${TEXT_SEPARATOR.DESCRIPTION.TO_STYLED_TEXT} ты можешь выбрать следующее место для постройки!`,
