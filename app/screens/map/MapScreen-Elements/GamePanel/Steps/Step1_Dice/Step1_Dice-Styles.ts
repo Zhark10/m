74c9a40562638e7dbe9a138821098287910b2263
Zhark10 <verygoodman18@yandex.ru>
2021-02-28 00:04:04 +0300

    step 1 styles updated

--- app/screens/map/MapScreen-Elements/GamePanel/Steps/Step1_Dice/Step1_Dice-Styles.ts
+++ app/screens/map/MapScreen-Elements/GamePanel/Steps/Step1_Dice/Step1_Dice-Styles.ts
@@ -1,14 +1,31 @@
 import { StyleSheet } from "react-native"
 import { color, spacing, typography } from "../../../../../../theme"
+import { screenWidth } from "../../../../../../utils/screen"
 import { COONTAINER_HEIGHT } from "../Step2_PlaceSelection/Step2_PlaceSelection-Styles"
 
 export const Step1Styles = StyleSheet.create({
+  COPYRIGHT: {
+    color: color.palette.white,
+    fontFamily: typography.primary.bold,
+    fontSize: 18,
+    textAlign: "center",
+  },
+  COPYRIGHT_BOX: {
+    backgroundColor: color.palette.gold,
+    borderRadius: 4,
+    bottom: -12,
+    fontSize: 18,
+    padding: 4,
+    position: 'absolute',
+  },
   DICE_CONTAINER: {
     alignItems: "center",
-    flex: 1,
+    borderRadius: 4,
+    borderWidth: 1,
     flexDirection: "row",
     height: COONTAINER_HEIGHT,
     justifyContent: "center",
+    width: screenWidth - 32,
   },
   INFO_CONTAINER: {
     marginVertical: spacing[3],
@@ -26,5 +43,24 @@ export const Step1Styles = StyleSheet.create({
     lineHeight: 32,
     paddingBottom: 4,
     textAlign: "center",
+  },
+  SIMULATED_TABLE: {
+    alignItems: "center",
+    backgroundColor: color.palette.white,
+    borderLeftColor: color.palette.gold,
+    borderLeftWidth: 2,
+    borderRadius: 4,
+    borderRightColor: color.palette.gold,
+    borderRightWidth: 2,
+    borderTopColor: color.palette.gold,
+    borderTopLeftRadius: screenWidth / 2,
+    borderTopRightRadius: screenWidth / 2,
+    borderTopWidth: 2,
+    bottom: 2,
+    flexDirection: "row",
+    height: (2 / 3) * COONTAINER_HEIGHT,
+    justifyContent: "center",
+    position: 'absolute',
+    width: screenWidth - 42
   },
 })
