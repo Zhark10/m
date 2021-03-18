0c7d17972d21ff15eac43a2db4d334750438fc93
Zhark10 <verygoodman18@yandex.ru>
2021-03-18 23:52:46 +0300

    game entity min refactor

--- app/models/entities/Game/Store.ts
+++ app/models/entities/Game/Store.ts
@@ -4,40 +4,40 @@ import { GameProgressModel } from "./Models"
 const coefficientForRadius = 0.4
 
 export const Store = types
-  .model({
+  .model('Game', {
     currentActivePlayer: types.maybeNull(types.string),
     gameProgress: GameProgressModel,
     isLoaded: types.boolean,
   })
-  .views((self) => ({
+  .views((game) => ({
     // FOR DYNAMIC MESSAGE SHOW
     get canBeCompletedStep1() {
-      const { first, second } = self.gameProgress.step1_DiceResult
+      const { first, second } = game.gameProgress.step1_DiceResult
       return Boolean(first && second)
     },
     get canBeCompletedStep2() {
-      return Boolean(self.gameProgress.step2_SelectedPlaceToBuild.placeId)
+      return Boolean(game.gameProgress.step2_SelectedPlaceToBuild.placeId)
     },
     get canBeCompletedStep3() {
-      return Boolean(self.gameProgress.step3_IsBuildStarted.isPlaceBuildStart)
+      return Boolean(game.gameProgress.step3_IsBuildStarted.isPlaceBuildStart)
     },
     get canBeCompletedStep4() {
-      return Boolean(self.gameProgress.step4_IsBuildFinished.IsBuildFinished)
+      return Boolean(game.gameProgress.step4_IsBuildFinished.IsBuildFinished)
     },
     get rollTheDiceResult() {
-      const { first, second } = self.gameProgress.step1_DiceResult
+      const { first, second } = game.gameProgress.step1_DiceResult
       return first + second
     },
     get calculatedRadius() {
-      const { first, second } = self.gameProgress.step1_DiceResult
+      const { first, second } = game.gameProgress.step1_DiceResult
       return (coefficientForRadius * (first + second)).toFixed()
     },
     get radiusInMeters() {
-      const { first, second } = self.gameProgress.step1_DiceResult
+      const { first, second } = game.gameProgress.step1_DiceResult
       return 1000 * coefficientForRadius * (first + second)
     },
     get stepForComplete() {
-      const { gameProgress } = self
+      const { gameProgress } = game
 
       if (gameProgress.step4_IsBuildFinished.IsBuildFinished) {
         return 4
@@ -51,12 +51,12 @@ export const Store = types
       return 1
     },
   }))
-  .actions((self) => ({
+  .actions((game) => ({
     saveDiceResult(cubeNumber: "first" | "second", value: number) {
-      self.gameProgress.step1_DiceResult[cubeNumber] = value
+      game.gameProgress.step1_DiceResult[cubeNumber] = value
     },
     resetDiceResult() {
-      self.gameProgress.step1_DiceResult = {
+      game.gameProgress.step1_DiceResult = {
         first: 0,
         second: 0,
         isCompleted: false,
